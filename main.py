2c4741a6afc8bd3067c405fa9901500c8d257afd
Стародубцев Степан <stepka.starodubtsev@mail.ru>
2021-12-26 17:54:44 +0700

    Основная работа проделана

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 from flask import Flask, request, jsonify
 import sympy as sym
-from sympy.abc import x
 import numpy as np
 import matplotlib.pyplot as plt
 import preparsing
 import random
+from flask_cors import CORS, cross_origin
+
 
 app = Flask(__name__)
+CORS(app)
+app.config['SECRET_KEY'] = 'secret'
+app.config['CORS_HEADERS'] = 'Content-Type'
     
 @app.route("/api/ode", methods=['POST'])
 def ode():
@@ -16,8 +20,6 @@ def ode():
         Принимает параметры:
             equation - уравнение (поддерживается пока только формат F(x, y, y', y'', и. т. д.) = 0) /строка/
             border - граничные условия (необязателен) /массив массивов вида [операнд, число x, правая часть], например, {["y", "0", "1"], ["y'", "0", "0"]}/
-            eq_type - тип уравнения (необязателен, пока на функцию не влияет)  /строка/
-            n - порядок уравнения (необязателен) /строка с целым числом/
             
         Отправляет в ответ:
             solution - массив выражений, являющихся решениями данного ДУ
@@ -26,35 +28,35 @@ def ode():
             
             err - описание
            
-        TO DO:
-            * прописать обработку и отправку данных в случае отсутствия решения уравнения
-            * добавить отправку переведённых в картинки формул
             
     '''
+    
     
     rez = {}
     
     _id = random.randint(1, 1000)
     print("Получен новый запрос, id="+str(_id))
-    req = request.get_json()
+    req = request.get_json(force=True)
+    print("["+str(_id)+"] "+str(request.get_data()))
     print(req)
     try:
         print("Поехали")
+        
         eq_str = preparsing.preparse(req['equation']) 
-        #if n == "":
-        #    eq_str = preparsing.preparse(req['equation']) 
-        #else:
-        #    eq_str = preparsing.preparse(req['equation'], n)
+        equation = sym.parse_expr(eq_str)
         print("["+str(_id)+"] Выражение успешно переписано: "+eq_str)
-        equation = sym.parse_expr(eq_str) 
         print("["+str(_id)+"] Выражение обработано успешно")
-        eq_type = req['eq_type']
         borders = {}
-        for b in req['border']:
-            borders[sym.sympify(b[0]+"("+b[1]+")")] = sym.sympyfy(b[2])
+        if req['border'] != []:
+            for b in req['border']:
+                if b[0] == "y":
+                    borders[sym.sympify(b[0]+"("+b[1]+")")] = sym.sympify(b[2])
+                elif b[0] == "y'":
+                    borders[sym.sympify(b[0][0]+"(x).diff(x).subs(x, "+b[1]+")")] = sym.sympify(b[2])
         print("["+str(_id)+"] Граничные условия обработаны успешно")
     
-    except NameError:
+    except (NameError, IndexError):
+        print("["+str(_id)+"] Ошибка при обработке введённых данных")
         rez["err"] = "Ошибка при обработке введённых данных"
         resp = jsonify(rez)
         resp.status_code = 400
@@ -63,25 +65,131 @@ def ode():
     x = sym.Symbol('x')
     y = sym.Function('y')(x)
     
+    
     try:
-        answer = sym.dsolve(equation, y, ics=borders)
-    except NameError:
+        if borders == {}:
+            print("Нет")
+            answer = sym.dsolve(equation, y) #, ics=borders)
+        else:
+            print("Есть")
+            answer = sym.dsolve(equation, y, ics=borders)
+    except (NameError, IndexError):
+        print("["+str(_id)+"] Ошибка во время решения уравнения")
         rez["err"] = "Ошибка во время решения уравнения"
         resp = jsonify(rez)
         resp.status_code = 400
         return resp
+    except ValueError:
+        rez["solution"] = ["Нет решения"]
+        resp = jsonify(rez)
+        resp.status_code = 200
+        print("["+str(_id)+"] Уравнение успешно решено")
+        print("["+str(_id)+"] Решения отправлены клиенту")
+        return resp
+    
+    print(sym.classify_ode(equation))
+    
+    print("["+str(_id)+"] Уравнение успешно решено")
     
     rez = {}
     solution = []
     if type(answer) == list:
         for a in answer:
-            solution.append(str(a.lhs)+"="+str(a.rhs))
+            solution.append(preparsing.postparse(str(a.lhs))+" = "+preparsing.postparse(str(a.rhs)))
     else:
-        solution.append(str(answer.lhs)+"="+str(answer.rhs))
+        solution.append(preparsing.postparse(str(answer.lhs))+" = "+preparsing.postparse(str(answer.rhs)))
     rez["solution"] = solution
     resp = jsonify(rez)
     resp.status_code = 200
+    print("["+str(_id)+"] Решения отправлены клиенту")
+    return resp
+
+@app.route("/api/sode", methods=['POST'])
+def sode():
+    '''
+        Решение систем ОДУ
+        
+        Принимает параметры:
+            equations - уравнение (поддерживается пока только формат F(x, y, y', y'', и. т. д.) = 0) /массив строк/
+            border - граничные условия (необязателен) /массив массивов вида [операнд, число x, правая часть], например, {["y", "0", "1"], ["y'", "0", "0"]}/
+            
+        Отправляет в ответ:
+            solution - массив выражений, являющихся решениями данного ДУ
+            
+            либо
+            
+            err - описание
+            
+    '''
+    rez = {}
+
+    _id = random.randint(1, 1000)
+    print("Получен новый запрос, id="+str(_id))
+    req = request.get_json()
+    
+    t = sym.Symbol('t')
+    x = sym.Function('x')(t)
+    y = sym.Function('y')(t)
+    
+    try:
+        print("Поехали")
+        
+        eqs_str = req['equations']
+        eqs = []
+        for e in eqs_str:
+            eqs.append(sym.parse_expr(preparsing.preparse(e, mode='sode'))) 
+        print("["+str(_id)+"] Выражения обработаны и переписаны успешно")
+        borders = {}
+        if req['border'] != []:
+        
+            for b in req['border']:
+                if b[0] == "y" or b[0] == "x":
+                    borders[sym.sympify(b[0]+"("+b[1]+")")] = sym.sympify(b[2])
+                elif b[0] == "y'" or b[0] == "x'":
+                    borders[sym.sympify(b[0][0]+"(t).diff(t).subs(t, "+b[1]+")")] = sym.sympify(b[2])
+    
+    except (NameError, IndexError):
+        print("["+str(_id)+"] Ошибка при обработке введённых данных")
+        rez["err"] = "Ошибка при обработке введённых данных"
+        resp = jsonify(rez)
+        resp.status_code = 400
+        return resp
+    
+
+    try:
+        if borders == {}:
+            answer = sym.dsolve(eqs, [x, y]) 
+        else:
+            answer = sym.dsolve(eqs, [x, y], ics=borders)
+    except (NameError, IndexError, NotImplementedError):
+        print("["+str(_id)+"] Ошибка во время решения системы")
+        rez["err"] = "Ошибка во время решения уравнения"
+        resp = jsonify(rez)
+        resp.status_code = 400
+        return resp
+    except ValueError:
+        rez["solution"] = ["Нет решения"]
+        resp = jsonify(rez)
+        resp.status_code = 200
+        print("["+str(_id)+"] Уравнение успешно решено")
+        print("["+str(_id)+"] Решения отправлены клиенту")
+        return resp
+    
+    print("["+str(_id)+"] Система успешно решена")
+    
+    rez = {}
+    solution = []
+    solution = []
+    if type(answer) == list:
+        for a in answer:
+            solution.append(preparsing.postparse(str(a.lhs))+" = "+preparsing.postparse(str(a.rhs)))
+    else:
+        solution.append(preparsing.postparse(str(answer.lhs))+" = "+preparsing.postparse(str(answer.rhs)))
+    rez["solution"] = solution
+    resp = jsonify(rez)
+    resp.status_code = 200
+    print("["+str(_id)+"] Решения отправлены клиенту")
     return resp
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5001)
+    app.run(debug=True, host='0.0.0.0', port=5000)
--- preparsing.py
+++ preparsing.py
@@ -1,4 +1,4 @@
-def preparse(string, n=""):
+def preparse(string, n="", mode="ode"):
 	# определяем, какого порядка ДУ, если не определено пользователем
     if n == "":
         n = 0
@@ -22,24 +22,45 @@ def preparse(string, n=""):
         if current > n:
             n = current
 
-    #print("Порядок: "+str(n))
+    if mode == "ode":
+        #обрабатываем все игреки на предмет того, что они функции
+        whereIs = []
+        for i, si in enumerate(string):
+            if si == 'y':
+                whereIs.append(i)
+        print(whereIs)
+        align = 0
+        for w in whereIs:
+            newstr = string[:w+1+align] + "(x)" + string[w+align+1:]
+            print(newstr)
+            string = newstr
+            align += 3
+        
+        #обрабатываем знаки производных
+        for col in range(n, 0, -1):
+            print("'")
+            string = string.replace("'" * col, ".diff(x"+",x"*(col-1)+")")
     
-    #обрабатываем все игреки на предмет того, что они функции
-    whereIs = []
-    for i, si in enumerate(string):
-        if si == 'y':
-            whereIs.append(i)
-    print(whereIs)
-    align = 0
-    for w in whereIs:
-        newstr = string[:w+1+align] + "(x)" + string[w+align+1:]
-        print(newstr)
-        string = newstr
-        align += 3
-    
-    #обрабатываем знаки производных
-    for col in range(n, 0, -1):
-        print("'")
-        string = string.replace("'" * col, ".diff(x"+",x"*(col-1)+")")
+    elif mode == "sode":
+        #обрабатываем все игреки на предмет того, что они функции
+        whereIs = []
+        for i, si in enumerate(string):
+            if si == 'y' or si == 'x':
+                whereIs.append(i)
+        print(whereIs)
+        align = 0
+        for w in whereIs:
+            newstr = string[:w+1+align] + "(t)" + string[w+align+1:]
+            print(newstr)
+            string = newstr
+            align += 3
+        
+        #обрабатываем знаки производных
+        for col in range(n, 0, -1):
+            print("'")
+            string = string.replace("'" * col, ".diff(t"+",t"*(col-1)+")")
     
     return string
+
+def postparse(string):
+    return string.replace("C", "C_").replace("**", "^")
